08ff79a5bbbabda385e097b7e340cdd9d3a6b634
rushi444 <rushi44@gmail.com>
2020-04-13 15:21:18 -0700

    cards component mvp

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import CountUp from 'react-countup';
-import { Grid, Box } from '@chakra-ui/core';
+import { Grid, Box, Spinner } from '@chakra-ui/core';
 import styled from '@emotion/styled';
 
 import { IVirusData } from '../types';
@@ -11,31 +11,73 @@ interface IProps {
 
 export const Cards: FC<IProps> = ({ data }) => {
   console.log(data);
-  !data ? (
-    <h1>Loading</h1>
-  ) : (
-    <Grid templateColumns='repeat(3, 1fr)' gap={6}>
-      <Box w='100%' h='100%' borderWidth='1px' rounded='10px' bg='#455a64'>
-        <BoxTitle>Infected</BoxTitle>
-        <BoxData>
-          <CountUp start={0} end={data?.confirmed.value} />
-        </BoxData>
-        <BoxDate>REAL DATE</BoxDate>
-        <BoxDef>Number of active cases of COVID-19</BoxDef>
-      </Box>
-      <Box w='100%' h='100%' borderWidth='1px' rounded='10px' bg='#455a64'>
-        <BoxTitle>Recovered</BoxTitle>
-        <BoxData>REAL DATA</BoxData>
-        <BoxDate>REAL DATE</BoxDate>
-        <BoxDef>Number of recoveries from COVID-19</BoxDef>
-      </Box>
-      <Box w='100%' h='100%' borderWidth='1px' rounded='10px' bg='#455a64'>
-        <BoxTitle>Infected</BoxTitle>
-        <BoxData>REAL DATA</BoxData>
-        <BoxDate>REAL DATE</BoxDate>
-        <BoxDef>Number of deaths from COVID-19</BoxDef>
-      </Box>
+
+  return !data ? (
+    <Grid templateColumns='repeat(1, 1fr)'>
+      <Spinner />
     </Grid>
+  ) : (
+    <>
+      <LastUpdateDate>
+        Last Updated: {new Date(data?.lastUpdate).toDateString()}
+      </LastUpdateDate>
+      <Grid templateColumns='repeat(3, 1fr)' gap={6}>
+        <Box
+          w='100%'
+          h='100%'
+          borderWidth='1px'
+          rounded='10px'
+          bg='#455a64'
+          style={{ borderBottom: '3px solid yellow' }}>
+          <BoxTitle>Infected</BoxTitle>
+          <BoxData>
+            <CountUp
+              start={0}
+              end={data?.confirmed.value || 0}
+              duration={2.5}
+              separator=','
+            />
+          </BoxData>
+          <BoxDef>Number of active cases of COVID-19</BoxDef>
+        </Box>
+        <Box
+          w='100%'
+          h='100%'
+          borderWidth='1px'
+          rounded='10px'
+          bg='#455a64'
+          style={{ borderBottom: '3px solid green' }}>
+          <BoxTitle>Recovered</BoxTitle>
+          <BoxData>
+            <CountUp
+              start={0}
+              end={data?.recovered.value || 0}
+              duration={2.5}
+              separator=','
+            />
+          </BoxData>
+          <BoxDef>Number of recoveries from COVID-19</BoxDef>
+        </Box>
+        <Box
+          w='100%'
+          h='100%'
+          borderWidth='1px'
+          rounded='10px'
+          bg='#455a64'
+          style={{ borderBottom: '3px solid red' }}>
+          <BoxTitle>Deaths</BoxTitle>
+          <BoxData>
+            <CountUp
+              start={0}
+              end={data?.deaths.value || 0}
+              duration={2.5}
+              separator=','
+            />
+          </BoxData>
+          <BoxDef>Number of fatalities from COVID-19</BoxDef>
+        </Box>
+      </Grid>
+    </>
   );
 };
 
@@ -43,14 +85,14 @@ const BoxTitle = styled.h1({
   textAlign: 'center',
 });
 
-const BoxData = styled.h3({
+const BoxData = styled.h2({
   textAlign: 'center',
 });
 
-const BoxDate = styled.h5({
+const LastUpdateDate = styled.h2({
   textAlign: 'center',
 });
 
-const BoxDef = styled.h5({
+const BoxDef = styled.h4({
   textAlign: 'center',
 });
